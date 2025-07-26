ef049da79586d9ed5a6a9814f479aadd0a987bfb
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-26 14:37:32 +0530

    if else if program

---
+++ C_Topic_07_Conditional_Statements/if_else_if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num = 0;
+    if (num > 0) {
+        printf("Positive number");
+    } else if (num < 0) {
+        printf("Negative number");
+    } else {
+        printf("Zero");
+    }
+    return 0;
+}
